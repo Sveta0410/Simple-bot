c1a9674fc1c782eb8be03ed98df8e8feadbf1d85
Sveta <sveta.zheleznova.03@mail.ru>
2021-11-19 15:45:37 +0300

    photo+question

--- main.py
+++ main.py
@@ -12,13 +12,20 @@ def start(message):
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Я ничего не умею :(')
-    bot.send_message(message.chat.id, 'Но скоро я многому научусь!!!')
+    bot.send_message(message.chat.id, 'Я смогу ответить на любой твой вопрос')
+    bot.send_message(message.chat.id, 'А ещё ты можешь прислать мне какую-нибудь классную картинку')
+
+@bot.message_handler(content_types=['photo'])
+def photo_r(message):
+    bot.send_photo(message.chat.id, open('/home/sveta/PycharmProjects/Simple-bot/picture.jpg', 'rb'))
 
 @bot.message_handler(content_types=['text'])
 def answer(message):
     if message.text.lower() == "хочу":
         bot.send_message(message.chat.id, 'Тогда тебе сюда – https://mtuci.ru/')
+    elif '?' in message.text.lower():
+        bot.send_message(message.chat.id, 'Гугли.')
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
